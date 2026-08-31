aa8645c25de844213e222cf6009ef60b534c4da4
aarrrggghhh <aner.moss@gmail.com>
2026-08-31 01:37:24 -0700

    first version of NFL routes shiny app

---
+++ NFL_routes/app.R
@@ -0,0 +1,95 @@
+library(shiny)
+library(bslib)
+library(tidyverse)
+
+source("helpers.R")
+
+files <- list.files(path = './data', pattern = '.rds')
+
+# read existing RDS files and create table of players we have data for
+# create vector of player names to feed "selectInput$player"
+
+ck2018app <- readRDS("./data/ck2018app.rds")
+ck2018app_results <- readRDS("./data/ck2018app_results.rds")
+
+# sidebar format for NFL route map app
+ui <- page_sidebar(
+            
+          title = "NFL route maps from 2018 season",
+          
+          sidebar = sidebar(
+            selectInput("player", "Player:", c("Cooper Kupp (LAR)" = "ck",
+                                               "Julio Jones (ATL)" = "jj",
+                                               "Davante Adams (GB)" = "da")
+                        ),
+            
+            radioButtons("plot_type", "Plot type:",
+                         choices = c("Routes" = "lines", "Density" = "density"), inline = TRUE
+                        ),
+                        
+            # Show these inputs only when plotting actual route maps (not density)
+            conditionalPanel("input.plot_type == 'lines'",
+                            card(
+                               card_header("Plot parameters"),
+                               div(
+                                 tags$style(".checkbox input {     width: .9em;
+                                                                   height: .9em;
+                                                                  }
+                                            label {font-size: .8rem;}"
+                                            ),
+                                     checkboxGroupInput("weeks",
+                                                "Weeks:",
+                                                choices = c(seq(1,17,1)),
+                                                selected = 1,
+                                                inline = TRUE
+                                      ),
+                                 div(
+                                   tags$style(".btn {--bs-btn-font-size: 80%;
+                                                    --bs-btn-padding-y: 2px;
+                                                    --bs-btn-padding-x: 8px;
+                                              }"),
+                                   actionButton("all", "All"),
+                                   actionButton("none", "None")
+                                 ),
+                                 br(),
+                                      checkboxInput("outcomes",
+                                           "Show play outcomes",
+                                           value = FALSE
+                                      )
+                                  ),
+                                actionButton("redraw", "Update")
+                             
+                           )
+          )
+       ),
+          
+           plotOutput("routes"),
+            textOutput("test")
+)
+
+
+
+server <- function(input, output, session) {
+
+# select all weeks button  
+    observeEvent(input$all, {
+      updateCheckboxGroupInput(session, "weeks", selected = c(seq(1,17,1)))
+    })
+  
+# select no weeks button
+    observeEvent(input$none, {
+      updateCheckboxGroupInput(session, "weeks", selected = 0)
+    })
+    
+    output$routes <- renderPlot({
+            switch(input$plot_type,
+                   "lines" = plot_routes(input$player, input$weeks, isTRUE(input$outcomes)),
+                   "density" = plot_density(input$player)
+            )
+    })  |>
+      bindEvent(c(input$redraw, input$plot_type, input$player), ignoreNULL = TRUE, ignoreInit = TRUE)
+
+}
+
+
+shinyApp(ui = ui, server = server)
---
+++ NFL_routes/helpers.R
@@ -0,0 +1,38 @@
+plot_routes <- function(player, weeks, outcomes) {
+  route_tbl <- paste0(player,"2018app")
+  route_data <- get(route_tbl)
+  select_routes <- route_data |> filter(week %in% weeks) |> arrange(uPlayId, frameId)
+  outcomes_data <- get(paste0(route_tbl,"_results"))
+  select_outcomes <- outcomes_data |> filter(week %in% weeks) |> arrange(uPlayId, frameId)
+  if(outcomes) {
+         ggplot() + 
+         geom_path(data = select_routes, aes(y, abs_x, group = uPlayId)) + 
+         coord_cartesian(ylim = c(-10,80), xlim = c(0, 53.3)) + 
+         theme_minimal() + 
+         theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_blank()) + 
+         geom_point(data = select_outcomes, aes(y, abs_x, color = outcome), cex = 3) + 
+         scale_color_manual(values = c("caught" = "#00BA38", "incomplete" = "#F8766D", "interception" = "red", "touchdown" = "purple", "touchdown catch" = "#619CFF"))
+    } else {
+         ggplot() + 
+         geom_path(data = select_routes, aes(y, abs_x, group = uPlayId)) + 
+         coord_cartesian(ylim = c(-10, 80), xlim = c(0, 53.3)) + 
+         theme_minimal() + 
+         theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_blank()) + 
+         geom_point(data = select_outcomes, aes(y, abs_x, color = outcome), cex = -1) +  # cex = -1 means these dots are hidden in output
+         scale_color_manual(values = c("#00BA38", "#F8766D", "red", "purple", "#619CFF"), guide = guide_legend(override.aes = list(alpha = 0))) + 
+         theme(legend.title = element_text(color = "transparent"), legend.text = element_text(color = "transparent"))  # hide the legend, but keep it there for plot sizing purposes
+        }
+}
+
+plot_density <- function(player) {
+        plot_data <- get(paste0(player,"2018app"))
+        full_name <- switch(player, "ck" = "Cooper Kupp", "jj" = "Julio Jones", "da" = "Davante Adams")
+        breaks <- exp(seq(log(0.00002), log(0.007), length.out = 80))
+        breaks <- c(0, breaks)
+        ggplot(data = plot_data, aes(y, abs_x)) + 
+          coord_cartesian(ylim = c(-10, 80), xlim = c(0, 53.3)) + 
+          geom_density_2d_filled(breaks = breaks) + 
+          theme_minimal() + 
+          theme(legend.position = "none", axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_blank()) + 
+          ggtitle(paste(full_name, " route density map - 2018 all games"))
+}
